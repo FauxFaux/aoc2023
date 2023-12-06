bcbc4ed6d7971bcddc3ce365f9b0172865017813
Chris West (Faux) <git@goeswhere.com>
2023-12-06 08:36:20 +0000

    d5p2 (brute)

--- src/d05.rs
+++ src/d05.rs
@@ -1,6 +1,8 @@
+use itertools::Itertools;
+
 pub fn solve() {
     let mut lines = include_str!("d05.txt").lines();
-    let seeds = lines
+    let pairs = lines
         .next()
         .expect("non-empty")
         .split_once(':')
@@ -8,7 +10,15 @@ pub fn solve() {
         .1
         .split_whitespace()
         .map(|s| s.parse::<i64>().expect("seed number"))
-        .collect::<Vec<_>>();
+        .tuples()
+        .collect::<Vec<(i64, i64)>>();
+
+    let mut seeds = Vec::with_capacity(99999);
+    for (l, d) in pairs {
+        for i in l..l + d {
+            seeds.push(i);
+        }
+    }
 
     let mut cats = Vec::new();
     let mut cat = Vec::new();
@@ -37,26 +47,39 @@ pub fn solve() {
     cats.push(cat);
     cats.remove(0);
 
-    let mut locs = Vec::new();
+    // let mut locs = Vec::new();
 
-    for seed in seeds {
-        let mut path = Vec::new();
-        let mut searching = seed;
+    let mut min = i64::MAX;
+
+    for (n, seed) in seeds.iter().enumerate() {
+        if n % 1_000_000 == 0 {
+            println!("{}/{}", n, seeds.len());
+        }
+        // let mut path = Vec::new();
+        let mut searching = *seed;
         for cat in &cats {
             let mut found = None;
             for (d, s, w) in cat {
                 if searching >= *s && searching < s + w {
                     found = Some(searching - s + d);
+                    break;
                 }
             }
             searching = found.unwrap_or(searching);
-            path.push(searching);
-            println!("step at: {path:?}");
+            // path.push(searching);
+            // println!("step at: {path:?}");
         }
 
-        println!("{seed} -> {path:?}");
-        locs.push(*path.last().expect("non-empty"))
+        // println!("{seed} -> {path:?}");
+        // let loc = *path.last().expect("non-empty");
+        let loc = searching;
+        if loc < min {
+            min = loc;
+        }
+        // locs.push(loc)
     }
 
-    println!("{locs:?} {}", locs.iter().min().expect("non-empty"));
+    // println!("{locs:?} {}", locs.iter().min().expect("non-empty"));
+
+    println!("{min}");
 }
