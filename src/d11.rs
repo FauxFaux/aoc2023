f15cf0c7695bd746e2090453721937aca8054958
Chris West (Faux) <git@goeswhere.com>
2023-12-11 08:48:14 +0000

    d11p2

--- src/d11.rs
+++ src/d11.rs
@@ -16,39 +16,6 @@ pub fn solve() {
         .filter(|x| (0..h).all(|y| !grid[y][*x]))
         .collect_vec();
 
-    let mut ng = grid.clone();
-    for y in nr.iter().rev() {
-        ng.insert(*y, vec![false; w]);
-    }
-
-    let h = ng.len();
-
-    println!("new rows:");
-    for y in 0..h {
-        for x in 0..w {
-            print!("{}", if ng[y][x] { "#" } else { "." });
-        }
-        println!();
-    }
-
-    for x in nc.iter().rev() {
-        for y in 0..h {
-            ng[y].insert(*x, false);
-        }
-    }
-
-    let w = ng[0].len();
-
-    println!("new cols:");
-    for y in 0..h {
-        for x in 0..w {
-            print!("{}", if ng[y][x] { "#" } else { "." });
-        }
-        println!();
-    }
-
-    let grid = ng;
-
     let mut coords = Vec::new();
     for y in 0..h {
         for x in 0..w {
@@ -58,17 +25,33 @@ pub fn solve() {
         }
     }
 
+    let g = 1000000-1;
+
+    let mut ng = coords.clone();
+    for gy in nr.iter().rev() {
+        for ((_, oy), (_, ny)) in coords.iter().zip(ng.iter_mut()) {
+            if oy > gy {
+                *ny += g;
+            }
+        }
+    }
+
+    for gx in nc.iter().rev() {
+        for ((ox, _), (nx, _)) in coords.iter().zip(ng.iter_mut()) {
+            if ox > gx {
+                *nx += g;
+            }
+        }
+    }
+
+    println!("{coords:?}");
+    println!("{ng:?}");
+
+    let coords = ng;
+
     let mut dists = Vec::new();
     let i6 = |v: usize| v as i64;
     let d = |a: usize, b: usize| (i6(a) - i6(b)).abs();
-    // for (ax, ay) in &coords {
-    //     for (bx, by) in &coords {
-    //         if ax == bx && ay == by {
-    //             continue;
-    //         }
-    //         dists.push(d(*ax, *bx) + d(*ay, *by) - 6);
-    //     }
-    // }
 
     let combs: Vec<((_, _), (_, _))> = coords.iter().copied().tuple_combinations().collect_vec();
     for ((ax, ay), (bx, by)) in combs {
