ae154a001bc687975cd97267089ffb610392fbb6
Chris West (Faux) <git@goeswhere.com>
2023-12-13 21:30:07 +0000

    d13p2: handle multiple results, and randomly change fences until it works

--- src/d13.rs
+++ src/d13.rs
@@ -24,37 +24,70 @@ pub fn solve() {
         println!();
 
         let search = |game: &Vec<Vec<bool>>| {
+            let mut hits = Vec::with_capacity(4);
             for y in 0..h - 1 {
-                if (0..=y).all(|d| {
+                if (0..h).all(|d| {
                     let o = d + 1;
                     y + o >= h || y < d || game[y - d] == game[y + o]
                 }) {
-                    return (0, y + 1);
+                    hits.push((0, y + 1));
                 }
             }
 
             for x in 0..w - 1 {
-                if (0..=x).all(|d| {
+                if (0..w).all(|d| {
                     let o = d + 1;
                     x + o >= w || x < d || (0..h).all(|y| game[y][x - d] == game[y][x + o])
                 }) {
-                    return (x + 1, 0);
+                    hits.push((x + 1, 0));
                 }
             }
 
-            return (0, 0);
+            hits
         };
 
-        let (x, y) = search(&game);
-        if x == 0 && y == 0 {
-            panic!("no solution");
+        let orig = search(&game);
+        assert_eq!(orig.len(), 1);
+        let orig = orig[0];
+
+        for fy in 0..h {
+            for fx in 0..w {
+                let mut game = game.clone();
+                game[fy][fx] = !game[fy][fx];
+                println!("{} {} {}", fx, fy, game[fy][fx]);
+                for y in 1..h - 1 {
+                    for x in 0..w {
+                        print!("{}", if game[y][x] { '#' } else { '.' });
+                    }
+                    if y == fy {
+                        print!("  <");
+                    }
+                    println!();
+                }
+                println!();
+                let now = search(&game)
+                    .into_iter()
+                    .filter(|c| *c != orig)
+                    .collect_vec();
+                if now.is_empty() {
+                    continue;
+                }
+                assert_eq!(now.len(), 1);
+                let (x, y) = now[0];
+                if x == 0 {
+                    score += y * 100;
+                    continue 'game;
+                } else {
+                    score += x;
+                    continue 'game;
+                }
+            }
         }
-        if x == 0 {
-            score += y * 100;
-        } else {
-            score += x;
-        }
+        panic!("no solution");
     }
 
+    // 26269 too low
+    // 34529 too low
+    // 37882 too low
     println!("{}", score);
 }
