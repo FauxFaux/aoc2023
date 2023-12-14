bc843f0f622efc307e268d10902f77cafadec679
Chris West (Faux) <git@goeswhere.com>
2023-12-14 22:10:55 +0000

    d14p2: finished on a calculator

--- src/d14.rs
+++ src/d14.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 pub fn solve() {
     let mut grid = include_str!("d14.txt")
@@ -9,35 +10,69 @@ pub fn solve() {
     let h = grid.len();
     let w = grid[0].len();
 
-    for _ in 0..h {
-        for sy in 1..h {
-            for x in 0..w {
-                if grid[sy][x] != 'O' {
-                    continue;
-                }
+    let mut grids = HashMap::new();
 
-                if grid[sy - 1][x] != '.' {
-                    continue;
+    for i in 0..1000000000 {
+        for (dx, dy) in [(0, -1), (-1, 0), (0, 1), (1, 0)] {
+            loop {
+                let mut changed = false;
+                for sy in 0..h {
+                    for sx in 0..w {
+                        if grid[sy][sx] != 'O' {
+                            continue;
+                        }
+
+                        let nx = sx as i64 + dx;
+                        let ny = sy as i64 + dy;
+                        if nx < 0 || nx >= w as i64 || ny < 0 || ny >= h as i64 {
+                            continue;
+                        }
+
+                        let nx = nx as usize;
+                        let ny = ny as usize;
+
+                        if grid[ny][nx] != '.' {
+                            continue;
+                        }
+                        grid[ny][nx] = 'O';
+                        grid[sy][sx] = '.';
+                        changed = true;
+                    }
                 }
-                grid[sy - 1][x] = 'O';
-                grid[sy][x] = '.';
+                if !changed {
+                    break;
+                }
             }
-
-            // for y in 0..h {
-            //     for x in 0..w {
-            //         print!("{}", grid[y][x]);
-            //     }
-            //     println!();
-            // }
-            // println!();
         }
+
+        grids
+            .entry(grid.clone())
+            .or_insert_with(|| Vec::new())
+            .push(i + 1);
+
+        if i % 800 == 799 {
+            println!("{}", i as f64 / 1000000000.0 * 100.0);
+            for (k, v) in &grids {
+                println!(
+                    "s: {}, l: {:?}",
+                    k.iter()
+                        .enumerate()
+                        .map(|(y, r)| (h - y) * r.iter().filter(|c| **c == 'O').count())
+                        .sum::<usize>(),
+                    v
+                );
+            }
+            return;
+        }
+        // for y in 0..h {
+        //     for x in 0..w {
+        //         print!("{}", grid[y][x]);
+        //     }
+        //     println!();
+        // }
+        // println!();
     }
 
-    let load: usize = grid
-        .iter()
-        .enumerate()
-        .map(|(y, r)| (h - y) * r.iter().filter(|c| **c == 'O').count())
-        .sum();
-
-    println!("{}", load);
+    // (1000000000-124)%(152-126)+124
+    // grep 124
 }
